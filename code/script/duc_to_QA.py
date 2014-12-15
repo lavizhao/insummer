56b1532800a344fa38bae1bbce6d2cf166cc2f69
CHarCH <charlley88@163.com>
2014-12-15 21:51:34 +0800

    duc

--- code/script/duc_to_QA.py
+++ code/script/duc_to_QA.py
@@ -3,13 +3,15 @@
 
 import re
 import pickle
-#import sys
-#sys.path.append("../code")
-#import insummer
+import sys
+sys.path.append("..")
+import insummer
 #from insummer.common_type import Question,Answer
-#from insummer.read_conf import config
+from insummer.read_conf import config
 
-def get_topic(topic_path,test_path):
+t_path = config('../../conf/question.conf')
+
+def get_topic(t_path):
     #REs for extract the topic items
     topic_re = re.compile(r'''<topic>(.+?)</topic>''',re.DOTALL)
     num_re = re.compile(r'''<num>(.+?)</num>''',re.DOTALL)
@@ -19,19 +21,22 @@ def get_topic(topic_path,test_path):
 
     print("开始读取topic文档...")
     str_list = ''
-    with open(topic_path,'r') as infile:
+    with open(t_path['duc_topic'],'r') as infile:
         for line in infile.readlines():
             if line != '\n':
-                str_list += line.rstrip()
+                str_list += line
         infile.close()
     print("开始抽取topic内容...")
     topic_str_list = topic_re.findall(str_list)
     
     for idx,item in enumerate(topic_str_list):
-        topic_num = num_re.findall(item)[0]
-        print(topic_num)
+        topic_dir_num = num_re.findall(item)[0]
+        topic_title = title_re.findall(item)[0]
+        topic_narr = narr_re.findall(item)[0]
+        topic_docs = doc_re.findall(item)[0].split('\n')[1:-1]
+        print(topic_docs)
 
 if __name__ == "__main__":
-    topic_path = '/home/charch/gitwork/insummer/duc/duc_data/duc2007_topics.sgml'
-    test_path = '/home/charch/gitwork/insummer/duc/duc_data/duc2007_testdocs/main/'
-    get_topic(topic_path,test_path)
+    #topic_path = '/home/charch/gitwork/insummer/duc/duc_data/duc2007_topics.sgml'
+    #test_path = '/home/charch/gitwork/insummer/duc/duc_data/duc2007_testdocs/main/'
+    get_topic(t_path)
