6e54fe91320da5ecef265c6ab39d8196e77ee31d
charch <charlley88@163.com>
2014-12-16 00:51:27 +0800

    get the title and the answers

--- code/script/duc_to_QA.py
+++ code/script/duc_to_QA.py
@@ -4,12 +4,29 @@
 import re
 import pickle
 import sys
-sys.path.append("..")
-import insummer
+#sys.path.append("..")
+#import insummer
 #from insummer.common_type import Question,Answer
-from insummer.read_conf import config
+#from insummer.read_conf import config
 
-t_path = config('../../conf/question.conf')
+#t_path = config('../../conf/question.conf')
+
+def doc_to_answer(doc_path):
+    answer_re = re.compile(r'''<TEXT>(.+?)</TEXT>''',re.DOTALL)
+    p_re = re.compile(r'''<P>(.+?)</P>''',re.DOTALL)
+    text_str = ''
+    with open(doc_path,'r') as infile:
+        for line in infile.readlines():
+            if line != '\n':
+                text_str += line
+        infile.close()
+    answer_part = answer_re.findall(text_str)[0]
+    p_list = p_re.findall(answer_part)
+    text_str = ''
+    for idx,p_line in enumerate(p_list):
+        #text_str += p_line.rstrip('\n')
+        text_str += p_line.replace('\n',' ')
+    return text_str
 
 def get_topic(t_path):
     #REs for extract the topic items
@@ -21,7 +38,7 @@ def get_topic(t_path):
 
     print("开始读取topic文档...")
     str_list = ''
-    with open(t_path['duc_topic'],'r') as infile:
+    with open(topic_path,'r') as infile:
         for line in infile.readlines():
             if line != '\n':
                 str_list += line
@@ -30,13 +47,27 @@ def get_topic(t_path):
     topic_str_list = topic_re.findall(str_list)
     
     for idx,item in enumerate(topic_str_list):
-        topic_dir_num = num_re.findall(item)[0]
+        #doc_topic_dir:
+        topic_dir_num = num_re.findall(item)[0].replace(' ','/')
+        #question_title:
         topic_title = title_re.findall(item)[0]
         topic_narr = narr_re.findall(item)[0]
+        #answer's document
         topic_docs = doc_re.findall(item)[0].split('\n')[1:-1]
-        print(topic_docs)
+
+        #construct the question:
+        question_title = topic_title + topic_narr
+
+        #construct the answer:
+        for doc_t in topic_docs:
+            doc_path = test_path + topic_dir_num + doc_t
+            single_answer = doc_to_answer(doc_path)
+            print(single_answer)
+            break
+        break
+
 
 if __name__ == "__main__":
-    #topic_path = '/home/charch/gitwork/insummer/duc/duc_data/duc2007_topics.sgml'
-    #test_path = '/home/charch/gitwork/insummer/duc/duc_data/duc2007_testdocs/main/'
-    get_topic(t_path)
+    topic_path = '/home/charch/gitwork/insummer/duc/duc_data/duc2007_topics.sgml'
+    test_path = '/home/charch/gitwork/insummer/duc/duc_data/duc2007_testdocs/main'
+    get_topic(topic_path)
